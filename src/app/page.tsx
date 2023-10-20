c8962e199a961fd8bc9912ed4ab2ee8df063c546
harsh9797 <patelharsh9797@gmail.com>
2023-10-20 15:45:52 +0530

    Added db string and fetched the data via tRPC

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+import { CreatePost } from "~/app/_components/create-post";
+import { api } from "~/trpc/server";
+
+export default async function Home() {
+  const allMessages = await api.post.getAllMessages.query();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="space-y-2">
+        {allMessages.map((message) => (
+          <p key={message.id}>{message.name}</p>
+        ))}
+      </div>
+
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <CrudShowcase />
+      </div>
+    </main>
+  );
+}
+
+async function CrudShowcase() {
+  const latestPost = await api.post.getLatest.query();
+
+  return (
+    <div className="w-full max-w-xs">
+      {latestPost ? (
+        <p className="truncate">Your most recent post: {latestPost.name}</p>
+      ) : (
+        <p>You have no posts yet.</p>
+      )}
+
+      <CreatePost />
+    </div>
+  );
+}
